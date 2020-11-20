2e723054ba21e211fdaa60a233b20223c0644d16
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-11-20 01:25:04 -0500

    add project

---
+++ AnimalKingdom/src/Animal.java
@@ -0,0 +1,53 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Animal
+{
+	private static int maxId = 0;
+
+	protected int id;
+
+	protected int yearDiscovered;
+
+	protected String name;
+
+	protected List<String> stomach = new ArrayList<>();
+
+	public Animal(
+			String name,
+			int yearDiscovered)
+	{
+		this.id = maxId++;
+		this.name = name;
+		this.yearDiscovered = yearDiscovered;
+	}
+
+	public int getYearDiscovered()
+	{
+		return yearDiscovered;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void eat(String food)
+	{
+		stomach.add(food);
+	}
+
+	abstract String move();
+
+	abstract String breath();
+
+	abstract String reproduce();
+
+	@Override
+	public String toString()
+	{
+		return "Animals{id=" + id + ", name=" + name + ", yearDiscovered=" + yearDiscovered + "}\n";
+	}
+}
---
+++ AnimalKingdom/src/Fish.java
@@ -0,0 +1,25 @@
+public class Fish extends Animal
+{
+	public Fish(
+			String name,
+			int yearDiscovered)
+	{
+		super(name,
+				yearDiscovered);
+	}
+
+	public String move()
+	{
+		return "swim";
+	}
+
+	public String breath()
+	{
+		return "gills";
+	}
+
+	public String reproduce()
+	{
+		return "eggs";
+	}
+}
